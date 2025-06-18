0cf480c783305f06b748dd301531f0cee2923b6e
rahul_r_432 <rahulramachandran432@gmail.com>
2025-06-18 15:37:42 +0530

    feat : server setup

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import cors from 'cors'
+import { configDotenv } from 'dotenv'
+
+configDotenv()
+
+const app = express()
+const PORT = process.env.PORT || 5555
+
+// middlewares
+app.use(cors)
+app.use(express.json())
+
+app.get('/', (req,res)=>{
+    console.log("Hello World")
+    res.json({message : "Backend Connected"})
+})
+
+app.listen(PORT,()=>{
+    console.log("Server Running Successfully")
+})
